f0f032719dc9ab218cc8904fa48270935d9a1f61
Connor Shearer <connor_shearer@outlook.com>
2022-07-22 15:07:15 +0100

    Add References with auto incrementing build

---
+++ Shared/References.cs
@@ -0,0 +1,13 @@
+﻿namespace Stryx.OS;
+
+public static class References
+{
+    public static string Application_Name { get; } = "StryxOS";
+    public static Version Application_Version { get; } = Runtime.Application_Shared_Assembly_Name != null && 
+                                                         Runtime.Application_Shared_Assembly_Name.Version != null ? 
+                                                         Runtime.Application_Shared_Assembly_Name.Version : new Version();
+    public static string Application_Version_Format_String { get; } = Runtime.Application_Shared_Assembly_Name != null ? 
+                                                                      $"{Application_Version.Major}.{Application_Version.Minor}.{Application_Version.Build}.{Application_Version.Revision}" : "Unknown";
+    public static string[] Application_Authors { get; } = new string[] { "Connor 'Stryxus' Shearer" };
+    public static string Application_Copyright { get; } = $"Copyright © {Application_Authors[0]} {DateTime.UtcNow.Year}";
+}
---
+++ Shared/Runtime.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace Stryx.OS;
+
+public static class Runtime
+{
+    public static Assembly Application_Shared_Assembly => typeof(Runtime).Assembly;
+    public static AssemblyName Application_Shared_Assembly_Name { get; } = typeof(Runtime).Assembly.GetName();
+}
